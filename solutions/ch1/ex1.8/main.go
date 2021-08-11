949e49c35ffe7ebf4a5b69391c42d133c2cbbfb5
Roman Kolesnikov <krn@pinroot.org>
2021-08-12 03:56:54 +0700

    Added solution ex1.8

---
+++ solutions/ch1/ex1.8/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	prefix := "http://"
+
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, prefix) {
+			// add prefix
+			url = prefix + url
+		}
+
+                resp, err := http.Get(url)
+                if err != nil {
+	                fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+        	        os.Exit(1)
+                }
+                _, err = io.Copy(os.Stdout, resp.Body)
+                resp.Body.Close()
+                if err != nil {
+              		fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+                        os.Exit(1)
+                }
+	}
+}
